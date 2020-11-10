1eacdcdd8e072ec54d5de72375d0ed6cde506177
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-11-10 14:32:06 -0300

    init results page

--- app/js/header.js
+++ app/js/header.js
@@ -4,6 +4,20 @@ let main_menu;
 let hamb_button;
 let close_menu;
 
+function setSearchLink() {
+    let search_button = document.querySelector("#search-button");
+    
+    let width = window.innerWidth;
+
+    if (width > 768){
+        search_button.href = "resultados.html";
+        return false;
+    }
+    
+    search_button.href = "buscar.html";
+
+}
+
 function openMainMenu() {
     main_menu.style.transform = `translateX(0vw)`;
 }
@@ -17,9 +31,12 @@ function initHeader() {
     main_menu = document.querySelector("#main-menu");
     hamb_button = document.querySelector("#hamb-button");
     close_menu = document.querySelector("#close-menu");
+    
+    setSearchLink();
 
     hamb_button.addEventListener("click", openMainMenu);
     close_menu.addEventListener("click", closeMainMenu);
+
 }
 
 document.addEventListener("DOMContentLoaded", initHeader);
