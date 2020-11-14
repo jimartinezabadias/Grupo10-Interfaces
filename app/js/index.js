fcc57ce15ba1e13c12c4c2519ec10dfe3e691e0a
AlfaroJoel <cholealfaro77@gmail.com>
2020-11-14 13:39:03 -0300

    login register ok

--- app/js/index.js
+++ app/js/index.js
@@ -7,29 +7,18 @@ let modal = document.querySelector(".modal");
 
 let width = window.innerWidth;
 
-if(window.location.pathname == "/app/index.html"){
-    btnComenzar.addEventListener("click", function(){
-        if(width > 768){
-            modal.style.display = "grid";
-            heroText.style.display = "none";
-        }else{
-            window.open("./login.html", "_self");
-        }
-    });
+btnComenzar.addEventListener("click", function(){
+    if(width > 768){
+        modal.style.display = "grid";
+        heroText.style.display = "none";
+    }else{
+        window.open("login.html", "_self");
+    }
+});
 
+if(width > 768){
     btnIngresar.addEventListener("click", function(){
         modal.style.display = "grid";
         heroText.style.display = "none";
     });
 }
-
-window.addEventListener("resize",function(){ 
-    width = window.innerWidth;
-    if(window.location.pathname == "/app/index.html"){
-        if(width < 768 && modal.style.display == "grid"){
-            window.open("./login.html", "_self");
-        }
-    }else if((window.location.pathname == "/app/login.html" || window.location.pathname == "/app/register.html") && width > 768){
-        window.open("./index.html", "_self");
-    }
-});
