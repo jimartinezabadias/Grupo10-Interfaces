82607f0feca953681ae76eadbb3f5b6d3f353b29
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-11-16 18:47:00 -0300

    delete open playing song

--- app/js/player.js
+++ app/js/player.js
@@ -1,27 +1,8 @@
 "use strict";
 
-let playing_song_button;
-let main;
-let playing_song_container;
-
-async function openPlayingSong() {
-    playing_song_container.style.transform = 'translateY(0)';
-    playing_song_container.style.visibility = 'visible';
-    await new Promise(r => setTimeout(r, 400));
-    main.style.display = 'none';
-}
-
 
 function initPlayer() {
     
-    let width = window.innerWidth;
-    main = document.querySelector('.main');
-    
-    if (width < 768){
-        playing_song_container = document.querySelector("#playing-song-container");
-        playing_song_button = document.querySelector("#open-playing-song");
-        playing_song_button.addEventListener("click",openPlayingSong);
-    }
 
 }
 
