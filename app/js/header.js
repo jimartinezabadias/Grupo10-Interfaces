9602eaeddba1a23b7a357784e5bc046e375bca86
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-11-17 20:20:28 -0300

    header de usuario

--- app/js/header.js
+++ app/js/header.js
@@ -4,19 +4,8 @@ let main_menu;
 let hamb_button;
 let close_menu;
 
-function setSearchLink() {
-    let search_button = document.querySelector("#search-button");
-    
-    let width = window.innerWidth;
-
-    if (width > 768){
-        search_button.href = "resultados.html";
-        return false;
-    }
-    
-    search_button.href = "buscar.html";
-
-}
+let user_menu_button;
+let user_menu;
 
 function openMainMenu() {
     main_menu.style.transform = `translateX(0vw)`;
@@ -26,17 +15,42 @@ function closeMainMenu() {
     main_menu.style.transform = `translateX(-100vw)`;
 }
 
+function openUserMenu() {
+    user_menu.style.transform = `translateX(0)`;
+}
+
+function closeUserMenu() {
+    user_menu.style.transform = `translateX(100%)`;
+}
+
+function handleUserMenu() {
+    if (user_menu.classList.contains('closed')){
+        openUserMenu();
+        user_menu.classList.remove("closed");
+        user_menu.classList.add("open");
+        return true;
+    }
+    closeUserMenu();
+    user_menu.classList.remove("open");
+    user_menu.classList.add("closed")
+    return true;    
+}
+
 function initHeader() {
     
     main_menu = document.querySelector("#main-menu");
     hamb_button = document.querySelector("#hamb-button");
     close_menu = document.querySelector("#close-menu");
     
-    setSearchLink();
-
     hamb_button.addEventListener("click", openMainMenu);
     close_menu.addEventListener("click", closeMainMenu);
+    
+    
+    user_menu_button = document.querySelector("#user-menu-btn");
+    user_menu = document.querySelector('#user-menu');
+    user_menu.classList.add("closed");
 
+    user_menu_button.addEventListener("click", handleUserMenu);
 }
 
 document.addEventListener("DOMContentLoaded", initHeader);
