8aa0a5dfeb536ab1c80666703e58ed53bed0a70e
Juan Ignacio Martínez Abadías <jimartinezabadias@gmail.com>
2020-11-22 19:36:45 -0300

    fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,18 @@
 let favboton = document.getElementById("fav-btn");
 let elemento = document.getElementById("elemento-agregado");
 let cora = document.getElementById("corazon-player");
+
 favboton.onclick = function() {
     elemento.classList.add("elemento-agregado");
     elemento.style.display = "flex";
     cora.setAttribute("fill", "#EB5757");
     cora.setAttribute("stroke", "#EB5757");
 };
+
+// Navbar
+let goBackButton = document.getElementById("nav-back-btn");
+
+goBackButton.addEventListener("click", goBack);
 
 function goBack() {
     window.history.back();
