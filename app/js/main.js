0158644a660274c4ae71fcedd77220d02fd43980
nahurup <nahuelruppel10@gmail.com>
2020-11-22 12:41:18 -0300

    favoritos en sidebar y player en template.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,11 @@
-let botonmenu = document.getElementById("boton-extensible");
-let menuextensible = document.getElementsByClassName("menu-extensible")[0];
-let flecha = document.getElementById("flecha");
-botonmenu.onclick = function() {
-    if(botonmenu.classList.contains("active")) {
-        botonmenu.classList.remove("active");
-        menuextensible.classList.add("ocultar");
-        flecha.style.transform = "rotate(180deg)";
-    }else if(!botonmenu.classList.contains("active")) {
-        botonmenu.classList.add("active");
-        menuextensible.classList.remove("ocultar");
-        flecha.style.transform = "rotate(0deg)";
-    }  
+let favboton = document.getElementById("fav-btn");
+let elemento = document.getElementById("elemento-agregado");
+let cora = document.getElementById("corazon-player");
+favboton.onclick = function() {
+    elemento.classList.add("elemento-agregado");
+    elemento.style.display = "flex";
+    cora.setAttribute("fill", "#EB5757");
+    cora.setAttribute("stroke", "#EB5757");
 };
 
 function goBack() {
